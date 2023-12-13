f9125eacf843b2000cbed4a0e9d76a5b65aef7d9
Sukhad <sukhadadhikari3@gmail.com>
2023-12-13 16:54:24 -0500

    First thing first

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+//normal function
+// function Greet()
+// {
+//     //returning from the function
+//    return  <h1>Hi Sukhad!</h1>
+// }
+
+
+const Greet = () => <h1>Hi sukhad</h1>//this is a inline function
+
+export default Greet;//exporting the function
